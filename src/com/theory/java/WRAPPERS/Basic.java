3b879c5f4e8fc3ef98db7b796605f9d02b788025
zhoy <artyomzhoy@gmail.com>
2021-07-28 17:43:30 +0300

    Wrappers

--- src/com/theory/java/WRAPPERS/Basic.java
+++ src/com/theory/java/WRAPPERS/Basic.java
@@ -30,3 +30,61 @@ public class Integer {
         return new Integer(x);
     }
 }
+
+/** Чтобы преобразовать тип int к типу Integer, нужно написать код:*/
+
+Integer имя = new Integer(оборачиваемое значение типа int, например 18 или i);
+
+    Integer age = new Integer(18);
+    int x = age.intValue();
+
+    int i = 110;
+    Integer zero = new Integer(i);
+    int z = zero.intValue();
+
+/**  Автоматическое преобразование int в Integer называется autoboxing (box — коробка, класть в коробку),
+а обратная операция — Integer к int — unboxing.
+Благодаря autoboxing и unboxing, можно спокойно присваивать тип int типу Integer и наоборот.*/
+
+class IntegerToInt {
+
+    Код:
+
+        Integer a = 5;
+        int b = a;
+        Integer c = a + b;
+        int d = a + b + c;
+
+    Что сгенерирует компилятор:
+
+        Integer a = Integer.valueOf(5);
+        int b = a.intValue();
+        Integer c = Integer.valueOf(a.intValue() + b);
+        int d = a.intValue() + b + c.intValue();
+}
+
+/** Если вы сравниваете Integer и Integer, происходит сравнение ссылок, а не значений.
+ * Переменные a и b хранят не значения (как типы int), а ссылки на объекты. Поэтому важно помнить, как правильно их сравнивать:
+ */
+
+class Equals {
+
+    Если вы сравниваете int и Integer, Integer преобразовывается в int:
+
+        Integer a = 1000;
+        Integer b = 1000;
+        int c = 500;
+
+        System.out.println(a == b); // сравнение ссылок - false. При autoboxing не всегда создается действительно новый объект Integer.
+                                    // Для значений от -128 до 127 включительно объекты кэшируются.
+                                    // При а и b = 100 результат был бы true.
+        System.out.println(a == c); // Integer a преобразовывается в int - true.
+        System.out.println(b == c); // Integer b преобразовывается в int - true.
+
+    Правильное сравнение ссылок:
+
+        Integer a = 1000;
+        Integer b = 1000;
+
+        System.out.println(a.equals(b));
+}
